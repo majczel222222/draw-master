7ad25ad0232c861362cc2157fce5bdf1b653e49f
majczel222222 <majczelgwizdala@gmail.com>
2015-05-29 11:26:24 +0200

    sasa

---
+++ draw/draw.cpp
@@ -0,0 +1,277 @@
+// draw.cpp : defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "draw.h"
+
+#define MAX_LOADSTRING 100
+#define TMR_1 1
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+INT value;
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+
+void MyOnPaint(HDC hdc)
+{
+	value++;
+	Graphics graphics(hdc);
+	SolidBrush p_pietro(Color::Brown);
+	Font font(&FontFamily(L"Arial"), 40);
+	Pen pen_winda(Color(255, 255, 0, 0), 3); 
+	Pen pen_sciana(Color(255, 0, 0, 0), 3);
+	Pen pen_lina(Color(255, 0, 0, 255), 2);
+	Pen pen_pietro(Color(255, 0, 0, 255), 9);
+
+	graphics.DrawLine(&pen_sciana, 97, 0, 97, 676.5); 
+	graphics.DrawLine(&pen_sciana, 158, 0, 158, 676.5);
+	graphics.DrawLine(&pen_lina, 127.5, 0 , 127.5, 10+value);
+
+	graphics.DrawLine(&pen_pietro, 45, 4.5, 210, 4.5);
+
+	graphics.DrawString(L"6", -1, &font, PointF(45, 42), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 100.5, 97, 100.5);
+	graphics.DrawLine(&pen_pietro, 158, 100.5, 210, 100.5);
+
+	graphics.DrawString(L"5", -1, &font, PointF(45, 138), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 196.5, 97, 196.5);
+	graphics.DrawLine(&pen_pietro, 158, 196.5, 210, 196.5);
+
+	graphics.DrawString(L"4", -1, &font, PointF(45, 234), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 292.5, 97, 292.5);
+	graphics.DrawLine(&pen_pietro, 158, 292.5, 210, 292.5);
+
+	graphics.DrawString(L"3", -1, &font, PointF(45, 330), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 388.5, 97, 388.5);
+	graphics.DrawLine(&pen_pietro, 158, 388.5, 210, 388.5);
+
+	graphics.DrawString(L"2", -1, &font, PointF(45, 426), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 484.5, 97, 484.5);
+	graphics.DrawLine(&pen_pietro, 158, 484.5, 210, 484.5);
+
+	graphics.DrawString(L"1", -1, &font, PointF(45, 522), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 580.5, 97, 580.5);
+	graphics.DrawLine(&pen_pietro, 158, 580.5, 210, 580.5);
+
+	graphics.DrawString(L"P", -1, &font, PointF(45, 618), &p_pietro);
+	graphics.DrawLine(&pen_pietro, 45, 676.5, 210, 676.5);
+
+
+	graphics.DrawRectangle(&pen_winda, 100, 10 + value, 55, 70);
+}
+
+
+int OnCreate(HWND window)
+{
+   SetTimer(window, TMR_1, 25, 0);
+   return 0;
+}
+
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	value= 10;
+
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_DRAW, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DRAW));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	GdiplusShutdown(gdiplusToken);
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DRAW));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_DRAW);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+   
+   OnCreate(hWnd);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	//OnCreate(hWnd,wParam,lParam);
+	//OnTimer(hWnd,wParam,lParam);
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		MyOnPaint(hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_TIMER:
+		switch (wParam)
+		{
+			case TMR_1:
+				//force window to repaint
+				InvalidateRect(hWnd, NULL, TRUE);
+				hdc = BeginPaint(hWnd, &ps);
+				MyOnPaint(hdc);
+				EndPaint(hWnd, &ps);
+			break;
+		}
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
